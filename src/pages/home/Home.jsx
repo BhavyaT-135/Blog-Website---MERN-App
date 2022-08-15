926a2ff9385f0c55364952ec9d590e834cf21c2a
BhavyaT-135 <bhavyatewari.13.5@gmail.com>
2022-08-15 21:03:04 +0530

    ⚡️ Created sidebar and posts

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,11 +1,16 @@
 import Header from '../../header/Header';
+import Posts from '../../posts/Posts';
+import Sidebar from '../../sidebar/Sidebar';
 import './home.css';
 
 export default function Home() {
   return (
+    <>
+      <Header />
       <div className="home">
-          <Header />
-          HomePage
+        <Posts />
+        <Sidebar />
       </div>
+    </>
   )
 }
