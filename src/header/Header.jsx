67b7a3957bf0e01dc170cece4b124f01074bbe59
BhavyaT-135 <bhavyatewari.13.5@gmail.com>
2022-08-15 20:53:49 +0530

    💄⚡️Header Design

---
+++ src/header/Header.jsx
@@ -0,0 +1,14 @@
+import './header.css'
+import homeImage from '../assets/sunset.jpg';
+
+export default function Header() {
+  return (
+    <div className="header">
+          <div className="headerTitles">
+              <span className='headerTitleSm'>React and Node</span>
+              <span className='headerTitleLg'>Blog</span>
+          </div>
+          <img className='headerImage' src={homeImage} alt='' />
+    </div>
+  )
+}
