4ffedbaf3aa98116151ebafd38b733181ca449a9
BhavyaT-135 <bhavyatewari.13.5@gmail.com>
2022-08-28 22:31:45 +0530

    ⚡️ 💄 Sidebar and About Section

--- src/sidebar/Sidebar.jsx
+++ src/sidebar/Sidebar.jsx
@@ -1,9 +1,34 @@
 import './sidebar.css';
+import aboutPic from '../assets/about.jpeg';
 
 export default function Sidebar() {
   return (
-      <div className='sidebar'>
-          Sidebar
+    <div className='sidebar'>
+      <div className='sidebarItem'>
+        <span className='sidebarTitle'>ABOUT ME</span>
+        <img src={aboutPic} alt="" />
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam euismod, nisi vel consectetur interdum, nisl nisi consectetur purus, eget egestas nisi nisi eget nisi.</p>
       </div>
+      <div className='sidebarItem'>
+        <span className='sidebarTitle'>CATEGORIES</span>
+        <ul className='sidebarList'>
+          <li className='sidebarListItem'>Life</li>
+          <li className='sidebarListItem'>Music</li>
+          <li className='sidebarListItem'>Style</li>
+          <li className='sidebarListItem'>Sport</li>
+          <li className='sidebarListItem'>Tech</li>
+          <li className='sidebarListItem'>Cinema</li>
+        </ul>
+      </div>
+      <div className='sidebarItem'>
+        <span className='sidebarTitle'>FOLLOW US</span>
+        <div className='sidebarSocial'>
+          <i className="sidebarIcon fab fa-facebook-square"></i>
+          <i className="sidebarIcon fab fa-twitter-square"></i>
+          <i className="sidebarIcon fa-brands fa-linkedin"></i>
+          <i className="sidebarIcon fa-brands fa-medium"></i> 
+        </div>
+      </div>
+    </div>
   )
 }
