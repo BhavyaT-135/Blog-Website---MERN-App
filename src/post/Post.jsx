fdefd9e3f34de2d5d73363f1068892e8710278a6
BhavyaT-135 <bhavyatewari.13.5@gmail.com>
2022-08-31 20:56:50 +0530

    💄 ⚡️ Post Component Styling

--- src/post/Post.jsx
+++ src/post/Post.jsx
@@ -1,20 +1,24 @@
 import './post.css'
+import postImg from '../assets/post.jpg'
 
 export default function Post() {
   return (
     <div className='post'>
       <img 
         className='postImg'
-        src=''
+        src={postImg}
         alt=''
       />
       <div className='postInfo'>
-        <div className='post Cats'>
+        <div className='postCats'>
           <span className='postCat'>Music</span>
           <span className='postCat'>Life</span>
         </div>
-        <span className='postTitle'></span>
+        <span className='postTitle'>Lorem ipsum dolor sit amet</span>
+        <hr />
+        <span className='postDate'> 1 hour ago</span>
       </div>
+      <p className='postDesc'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam euismod, nisi vel consectetur interdum, nisl nisi consectetur purus, eget egestas nisi nisi eget nisi. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam euismod, nisi vel consectetur interdum, nisl nisi consectetur purus, eget egestas nisi nisi eget nisi. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam euismod, nisi vel consectetur interdum, nisl nisi consectetur purus, eget egestas nisi nisi eget nisi.</p>
     </div>
   )
 }
