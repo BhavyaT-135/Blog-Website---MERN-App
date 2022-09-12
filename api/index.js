b58649bb753f41542e884fd68960eed7d7f5d92f
BhavyaT-135 <bhavyatewari.13.5@gmail.com>
2022-09-13 03:11:00 +0530

    ✨ Route for registration

--- api/index.js
+++ api/index.js
@@ -2,13 +2,17 @@ const express = require('express');
 const app = express();
 const dotenv = require('dotenv');
 const mongoose = require('mongoose');
+const authRoute = require('./routes/auth');
 
 dotenv.config();
+app.use(express.json());
 
 mongoose.connect(process.env.MONGO_URL, {
     useNewUrlParser: true,
     useUnifiedTopology: true
 }).then(console.log("Connected to MongoDB")).catch((err) => console.log(err));
+
+app.use("/api/auth", authRoute);
 
 app.listen(5000, () => {
     console.log('Server started on port 5000');
