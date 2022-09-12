f1ff0a5cf8c7073bda38ae77bf8c25a99b2016d4
BhavyaT-135 <bhavyatewari.13.5@gmail.com>
2022-09-12 22:27:43 +0530

    ✨ MongoDB Connection Setup

--- api/index.js
+++ api/index.js
@@ -1,5 +1,14 @@
 const express = require('express');
 const app = express();
+const dotenv = require('dotenv');
+const mongoose = require('mongoose');
+
+dotenv.config();
+
+mongoose.connect(process.env.MONGO_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+}).then(console.log("Connected to MongoDB")).catch((err) => console.log(err));
 
 app.listen(5000, () => {
     console.log('Server started on port 5000');
