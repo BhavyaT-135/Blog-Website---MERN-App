cc0b52f603cd67c349e0501438263b1cba862749
BhavyaT-135 <bhavyatewari.13.5@gmail.com>
2022-09-14 12:57:33 +0530

    ⚡️ Not displaying password in api

--- api/routes/auth.js
+++ api/routes/auth.js
@@ -36,7 +36,8 @@ router.post("/login", async (req, res) => {
             return
         }
 
-        res.status(200).json(user);
+        const { password, ...others } = user._doc;
+        res.status(200).json(others);
     }
     catch(err) {
         res.status(500).json(err); 
