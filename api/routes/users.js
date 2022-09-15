2ef3692d028a02c2f896a8499cc0323c4602b9d8
BhavyaT-135 <bhavyatewari.13.5@gmail.com>
2022-09-15 10:28:06 +0530

    ✨ GET User Route

--- api/routes/users.js
+++ api/routes/users.js
@@ -45,4 +45,15 @@ router.delete('/:id', async (req, res) => {
     }
 });
 
+//GET
+router.get('/:id', async (req, res) => {
+    try {
+        const user = await User.findById(req.params.id);
+        const { password, ...others } = user._doc;
+        res.status(200).json(others);
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+
 module.exports = router;
