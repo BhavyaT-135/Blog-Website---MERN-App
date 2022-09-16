0b51e9c7feff8d73a0b9830e471c1252f3756183
BhavyaT-135 <bhavyatewari.13.5@gmail.com>
2022-09-16 19:25:31 +0530

    ✨ Update Post Route

--- api/routes/posts.js
+++ api/routes/posts.js
@@ -14,6 +14,29 @@ router.post('/', async (req, res) => {
 });
 
 //UPDATE POST
+router.put('/:id', async(req, res) => {
+    const post = await Post.findById(req.params.id);
+    try {
+        if (post) {
+            if (post.username === req.body.username) {
+                try {
+                    const updatedPost = await Post.findByIdAndUpdate(req.params.id, {
+                        $set: req.body,
+                    }, { new: true });
+                    res.status(200).json(updatedPost);
+                } catch (err) {
+                    res.status(500).json(err);
+                }
+            } else {
+                res.status(401).json("You can only update your post!");
+            }
+        }
+    } catch(err) {
+        res.status(500).json(err);
+    }
+})
+
+
 //DELETE POST
 //GET POST
 
