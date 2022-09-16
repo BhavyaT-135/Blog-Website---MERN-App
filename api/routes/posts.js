e64377aa3a59ca1d53e4b7984c555374c90284c2
BhavyaT-135 <bhavyatewari.13.5@gmail.com>
2022-09-16 19:20:48 +0530

    ✨ Create Post Route

---
+++ api/routes/posts.js
@@ -0,0 +1,20 @@
+const router = require('express').Router();
+const User = require('../models/User');
+const Post = require('../models/Post');
+
+//CREATE POST
+router.post('/', async (req, res) => {
+    const newPost = new Post(req.body);
+    try {
+        const savedPost = await newPost.save();
+        res.status(200).json(savedPost);
+    } catch(err) {
+        res.status(500).json(err);
+    }
+});
+
+//UPDATE POST
+//DELETE POST
+//GET POST
+
+module.exports = router;
