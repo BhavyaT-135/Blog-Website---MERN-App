616382323dcc1c009652df7714262ef005d53814
BhavyaT-135 <bhavyatewari.13.5@gmail.com>
2022-09-16 19:38:13 +0530

    ✨ Delete Post Route

--- api/routes/posts.js
+++ api/routes/posts.js
@@ -36,8 +36,26 @@ router.put('/:id', async(req, res) => {
     }
 })
 
-
 //DELETE POST
+router.delete('/:id', async (req, res) => {
+    try {
+        const post = await Post.findById(req.params.id);
+        if (post.username === req.body.username) {
+            try {
+                await post.delete();
+                res.status(200).json("Post has been deleted...");
+            } catch (err) {
+                res.status(500).json(err);
+            }
+        }
+        else {
+            res.status(401).json("You can only delete your post!");
+        }
+    } catch (err) {
+        res.status(500).json(err);
+    }
+})
+
 //GET POST
 
 module.exports = router;
