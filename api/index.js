6466d169d83596cdae939eb6f12b76b97f7535cf
BhavyaT-135 <bhavyatewari.13.5@gmail.com>
2022-09-17 20:16:38 +0530

    ✨ Category Routes

--- api/index.js
+++ api/index.js
@@ -5,6 +5,7 @@ const mongoose = require('mongoose');
 const authRoute = require('./routes/auth');
 const userRoute = require('./routes/users');
 const postRoute = require('./routes/posts');
+const categoryRoute = require('./routes/categories');
 
 dotenv.config();
 app.use(express.json());
@@ -17,6 +18,8 @@ mongoose.connect(process.env.MONGO_URL, {
 app.use("/api/auth", authRoute);
 app.use("/api/users", userRoute);
 app.use("/api/posts", postRoute);
+app.use("/api/categories", categoryRoute);
+
 
 app.listen(5000, () => {
     console.log('Server started on port 5000');
