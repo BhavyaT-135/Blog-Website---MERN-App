066be9f765de27567066216f1a625c41e40ba5ae
BhavyaT-135 <bhavyatewari.13.5@gmail.com>
2022-10-11 19:55:19 +0530

    ⚡️ Registration State

--- client/src/pages/register/Register.jsx
+++ client/src/pages/register/Register.jsx
@@ -1,22 +1,41 @@
 import "./register.css"
-import {Link} from 'react-router-dom'
+import { Link } from 'react-router-dom'
+import { useState } from 'react'
 
 export default function Register() {
-    return (
-    <div className="register">
-      <span className="registerTitle">Register</span>
-      <form className="registerForm">
-        <label>Username</label>
-        <input className="registerInput" type="text" placeholder="Enter your username..." />
-        <label>Email</label>
-        <input className="registerInput" type="text" placeholder="Enter your email..." />
-        <label>Password</label>
-        <input className="registerInput" type="password" placeholder="Enter your password..." />
-        <button className="registerButton">Register</button>
-      </form>
-    <button className="registerLoginButton">
-        <Link className="link" to="/login">LOGIN</Link>
-    </button>
-    </div>
-    )
+  const [username, setUsername] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  return (
+  <div className="register">
+    <span className="registerTitle">Register</span>
+    <form className="registerForm">
+      <label>Username</label>
+      <input
+        className="registerInput"
+        type="text"
+        placeholder="Enter your username..." 
+        onChange={(e) => setUsername(e.target.value)}
+        />
+      <label>Email</label>
+      <input
+        className="registerInput"
+        type="text"
+        placeholder="Enter your email..." 
+        onChange={(e) => setEmail(e.target.value)}
+        />
+      <label>Password</label>
+      <input
+        className="registerInput"
+        type="password"
+        placeholder="Enter your password..." 
+        onChange={(e) => setPassword(e.target.value)}
+        />
+      <button className="registerButton">Register</button>
+    </form>
+  <button className="registerLoginButton">
+      <Link className="link" to="/login">LOGIN</Link>
+  </button>
+  </div>
+  )
 }
