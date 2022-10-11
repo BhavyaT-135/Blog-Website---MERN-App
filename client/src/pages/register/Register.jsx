0f6f17c8360c1b735b05654733ec6a008b16f9db
BhavyaT-135 <bhavyatewari.13.5@gmail.com>
2022-10-11 22:48:50 +0530

    ✨ Functioning Registration Page

--- client/src/pages/register/Register.jsx
+++ client/src/pages/register/Register.jsx
@@ -1,15 +1,33 @@
 import "./register.css"
 import { Link } from 'react-router-dom'
 import { useState } from 'react'
+import axios from 'axios' 
 
 export default function Register() {
   const [username, setUsername] = useState("");
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const [error, setError] = useState(false);
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    setError(false);
+    try {
+      const res = axios.post("/auth/register", {
+        username,
+        email,
+        password,
+      });
+      res.data && window.location.replace("/login");
+    } catch (err) {
+      setError(true);
+    }
+  };
+
   return (
   <div className="register">
     <span className="registerTitle">Register</span>
-    <form className="registerForm">
+    <form className="registerForm" onSubmit={handleSubmit}>
       <label>Username</label>
       <input
         className="registerInput"
@@ -31,11 +49,12 @@ export default function Register() {
         placeholder="Enter your password..." 
         onChange={(e) => setPassword(e.target.value)}
         />
-      <button className="registerButton">Register</button>
+      <button className="registerButton" type="submit">Register</button>
     </form>
   <button className="registerLoginButton">
       <Link className="link" to="/login">LOGIN</Link>
-  </button>
+      </button>
+      {error && <span style = {{color: "red", marginTop: "10px"}}>Something went wrong!</span>}
   </div>
   )
 }
