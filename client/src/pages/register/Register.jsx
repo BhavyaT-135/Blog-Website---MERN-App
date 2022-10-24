3e1506195d0d925ae8e9dd4eb029572ecb749191
BhavyaT-135 <bhavyatewari.13.5@gmail.com>
2022-10-24 22:17:23 +0530

    🐛 Resolving issue for Login system

--- client/src/pages/register/Register.jsx
+++ client/src/pages/register/Register.jsx
@@ -1,7 +1,7 @@
-import "./register.css"
-import { Link } from 'react-router-dom'
-import { useState } from 'react'
-import axios from 'axios' 
+import axios from "axios";
+import { useState } from "react";
+import { Link } from "react-router-dom";
+import "./register.css";
 
 export default function Register() {
   const [username, setUsername] = useState("");
@@ -13,7 +13,7 @@ export default function Register() {
     e.preventDefault();
     setError(false);
     try {
-      const res = axios.post("/auth/register", {
+      const res = await axios.post("/auth/register", {
         username,
         email,
         password,
@@ -23,38 +23,41 @@ export default function Register() {
       setError(true);
     }
   };
-
   return (
-  <div className="register">
-    <span className="registerTitle">Register</span>
-    <form className="registerForm" onSubmit={handleSubmit}>
-      <label>Username</label>
-      <input
-        className="registerInput"
-        type="text"
-        placeholder="Enter your username..." 
-        onChange={(e) => setUsername(e.target.value)}
+    <div className="register">
+      <span className="registerTitle">Register</span>
+      <form className="registerForm" onSubmit={handleSubmit}>
+        <label>Username</label>
+        <input
+          type="text"
+          className="registerInput"
+          placeholder="Enter your username..."
+          onChange={(e) => setUsername(e.target.value)}
         />
-      <label>Email</label>
-      <input
-        className="registerInput"
-        type="text"
-        placeholder="Enter your email..." 
-        onChange={(e) => setEmail(e.target.value)}
+        <label>Email</label>
+        <input
+          type="text"
+          className="registerInput"
+          placeholder="Enter your email..."
+          onChange={(e) => setEmail(e.target.value)}
         />
-      <label>Password</label>
-      <input
-        className="registerInput"
-        type="password"
-        placeholder="Enter your password..." 
-        onChange={(e) => setPassword(e.target.value)}
+        <label>Password</label>
+        <input
+          type="password"
+          className="registerInput"
+          placeholder="Enter your password..."
+          onChange={(e) => setPassword(e.target.value)}
         />
-      <button className="registerButton" type="submit">Register</button>
-    </form>
-  <button className="registerLoginButton">
-      <Link className="link" to="/login">LOGIN</Link>
+        <button className="registerButton" type="submit">
+          Register
+        </button>
+      </form>
+      <button className="registerLoginButton">
+        <Link className="link" to="/login">
+          Login
+        </Link>
       </button>
-      {error && <span style = {{color: "red", marginTop: "10px"}}>Something went wrong!</span>}
-  </div>
-  )
+      {error && <span style={{color:"red", marginTop:"10px"}}>Something went wrong!</span>}
+    </div>
+  );
 }
