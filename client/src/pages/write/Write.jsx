98dc4394a22150f7fda4710b1e523bfa1d975173
BhavyaT-135 <bhavyatewari.13.5@gmail.com>
2022-11-04 11:11:31 +0530

    ⚡️ Adding file to Write page

--- client/src/pages/write/Write.jsx
+++ client/src/pages/write/Write.jsx
@@ -1,7 +1,36 @@
 import './write.css'
 import postImg from '../../assets/post.jpg'
+import axios from 'axios'
+import { useContext, useRef, useState } from 'react'
+import { Context } from '../../context/Context'
 
 export default function Write() {
+    const [ title, setTitle ] = useState('')
+    const [desc, setDesc] = useState('')
+    const [file, setFile] = useState(null)
+    const {user} = useContext(Context)
+
+    const handleSubmit = (e) => {
+        e.preventDefault()
+        const newPost = {
+            username: user.username,
+            title,
+            desc
+        }
+        if(file) {
+            const data = new FormData()
+            const filename = Date.now() + file.name
+            data.append("name", filename)
+            data.append("file", file)
+            newPost.photo = filename
+            try {
+                axios.post("/upload", data)
+            } catch (err) {
+                
+            }
+        }
+        axios.post("/posts")
+    }
   return (
       <div className='write'>
           <img 
@@ -9,7 +38,7 @@ export default function Write() {
               src={postImg}
               alt=''
           />
-          <form className='writeForm'>
+          <form className='writeForm' onSubmit={handleSubmit}>
               <div className='writeFormGroup'>
                   <label htmlFor='fileInput'>
                       <i className='writeIcon fas fa-plus'></i>
@@ -20,7 +49,7 @@ export default function Write() {
               <div className='writeFormGroup'>
                   <textarea placeholder='Tell your story...' type='text' className='writeInput writeText'></textarea>
               </div>
-              <button className='writeSubmit'>Publish</button>
+              <button className='writeSubmit' type="submit">Publish</button>
           </form>
       </div>
   )
