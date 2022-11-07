7647867023ad3a796cca795ca2de20e1088eb225
BhavyaT-135 <bhavyatewari.13.5@gmail.com>
2022-11-07 12:22:49 +0530

    ⚡️ Write page image

--- api/index.js
+++ api/index.js
@@ -7,13 +7,17 @@ const userRoute = require('./routes/users');
 const postRoute = require('./routes/posts');
 const categoryRoute = require('./routes/categories');
 const multer = require('multer');
+const path = require('path');
 
 dotenv.config();
 app.use(express.json());
+app.use('/images', express.static(path.join(__dirname, '/images')));
 
 mongoose.connect(process.env.MONGO_URL, {
     useNewUrlParser: true,
-    useUnifiedTopology: true
+    useUnifiedTopology: true,
+    // useCreateIndex: true,
+    // useFindAndModify: false
 }).then(console.log("Connected to MongoDB")).catch((err) => console.log(err));
 
 const storage = multer.diskStorage({
