5a5d3e50109f691c1f8b55e0e14768516cc4ddac
BhavyaT-135 <bhavyatewari.13.5@gmail.com>
2023-01-02 18:36:30 +0530

    🚀 Vercel Deployment

--- api/index.js
+++ api/index.js
@@ -40,6 +40,12 @@ app.use("/api/users", userRoute);
 app.use("/api/posts", postRoute);
 app.use("/api/categories", categoryRoute);
 
+__dirname = path.resolve();
+
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, '../client/build')));
+}
+
 
 app.listen(5000, () => {
     console.log('Server started on port 5000');
